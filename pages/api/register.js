52dad3ac240cd46dd9b00718c972f30d82ec292d
vkm41101 <vkm41101@gmmail.com>
2023-06-06 01:28:45 +0530

    working on register backendddds

---
+++ pages/api/register.js
@@ -0,0 +1,10 @@
+import userObject from '../../models/user'
+
+export default function respond(req, res)
+{
+    if(req.method==='POST')
+    {
+        let data=JSON.parse(req.body);
+        
+    }
+}
